e73e6b5e29530b9d6c1f16ab83c8b884e3c0025a
mortdecai40 <rizkinabil.999@gmail.com>
2020-10-16 14:33:53 +0700

    Program logika

---
+++ src/pbo2/pkg10116374/latihan15/logika/PBO210116374Latihan15Logika.java
@@ -0,0 +1,21 @@
+package pbo2.pkg10116374.latihan15.logika;
+
+/** @author
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk melakukan Proses Logika
+ *
+ **/
+public class PBO210116374Latihan15Logika {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println(" a && b \t= " + (a && b));
+        System.out.println(" a || b \t= " + (a && b));
+        System.out.println(" !a \t\t= " + !a);
+        System.out.println(" !(a && b) \t= " + (a && b));
+    }
+    
+}
